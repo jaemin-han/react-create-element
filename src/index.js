a226867f5800d15e6e74de0e82cea73b298a0ed7
jaemin.han@nyu.edu <jaemin.han@nyu.edu>
2018-03-30 15:43:28 -0400

    complete steps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// TEST
+// const title = React.createElement('h1', {}, 'My First React Code');
+// const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+// const container = React.createElement('div', {}, [title, paragraph]);
+// passing down sibling from container
+
+
+const list = 
+    React.createElement('div', {}, 
+    React.createElement('h1', {}, 'My favorite ice cream flavor'),
+    React.createElement('ul', {},
+        [
+            React.createElement('li', { className: 'brown'}, 'Chocolate'),
+            React.createElement('li', { className: 'white' }, 'Vanila'),
+            React.createElement('li', { className: 'yellow' }, 'Banana')
+        ]
+    )
+);
+
+
+
+
+
+ReactDOM.render(
+    list,
+    document.getElementById('global')
+)
